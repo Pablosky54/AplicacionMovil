c96616acb51bed3c786380fff46b43ea5d773b13
Pablo <jpg-05-12@hotmail.com>
2026-01-19 01:17:38 -0500

    Se crea el actor y la habilidad de abrir la APK

---
+++ FrisbyTest/src/test/java/com/frisby/utils/Actor.java
@@ -0,0 +1,30 @@
+package com.example.frisby.screenplay;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Actor {
+    private final String name;
+    private final Map<Class<?>, Object> abilities = new HashMap<>();
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    public static Actor named(String name) {
+        return new Actor(name);
+    }
+
+    public <T> Actor can(T ability) {
+        abilities.put(ability.getClass(), ability);
+        return this;
+    }
+
+    public <T> T abilityTo(Class<T> abilityClass) {
+        return abilityClass.cast(abilities.get(abilityClass));
+    }
+
+    public String getName() {
+        return name;
+    }
+}
